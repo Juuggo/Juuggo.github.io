f56698b196095e6ccc574f2d1fa2a1e57b92ccc9
Juuggo <Juuggo.c@gmail.com>
2019-01-28 21:56:01 +0300

    Made tag index linking to tag and post items.

--- js/tagIndex.js
+++ js/tagIndex.js
@@ -1,13 +1,17 @@
 class TagNav {
     constructor() {
-        this.navBar = document.getElementsByClassName('tag-nav')[0];
-        this.tagList = document.getElementsByClassName('tag-post-list')[0];
-        this.tagItems = document.getElementsByClassName('tag-post-item');
+        this.tagLinks = document.getElementsByClassName('tag-list')[0].getElementsByTagName('a');
+        this.itemContainer = document.getElementsByClassName('tag-item-container')[0];
+        let tagItems = document.getElementsByClassName('tag-item');
+        this.tagItems = Array.prototype.slice.call(tagItems);
         this.itemWidth = parseInt(window.getComputedStyle(this.tagItems[0]).width); 
-        this.currentItemIndex = 0;
         this.totalTagCounts = this.tagItems.length;
     }
     initItemPosition() {
+        console.log('start init postion, index:' + this.currentItemIndex);
+        this.itemContainer.style.left = -this.currentItemIndex * this.itemWidth + 'px'; 
+        console.log(this.itemContainer.style.left);
+        console.log('end init postion, index:' + this.currentItemIndex);
         for(let i=0; i<this.totalTagCounts; i++) {
             let itemStyle = this.tagItems[i].style;
             itemStyle.left = this.itemWidth * i + 'px';
@@ -15,28 +19,54 @@ class TagNav {
     }
     scrollLeft() {
         if (this.currentItemIndex !== this.totalTagCounts - 1) {
-            this.tagList.style.left = -(this.currentItemIndex+1) * this.itemWidth + 'px'; 
+            this.itemContainer.style.left = -(this.currentItemIndex+1) * this.itemWidth + 'px'; 
             this.currentItemIndex++;
         }
     }
     scrollRight() {
         if (this.currentItemIndex !== 0) {
-            this.tagList.style.left = -(this.currentItemIndex-1) * this.itemWidth + 'px'; 
+            this.itemContainer.style.left = -(this.currentItemIndex-1) * this.itemWidth + 'px'; 
             this.currentItemIndex--;
         }
     }
-    scrollToTag(toTagName) {
-        let currentItem = this.tagList[this.currentItemIndex];
-        let currentTag = currentItem.getAttribute('id').split('-')[1];
-        console.log(currentTag);
-        if (currentTag !== toTagName) {
-
+    getTagIndexByName(tagName) {
+        let toItem = document.getElementById(tagName);
+        let tagIndex = this.tagItems.indexOf(toItem);
+        return tagIndex;
+    }
+    scrollToTag(tagName) {
+        let tagIndex = this.getTagIndexByName(tagName);
+        console.log(this.itemWidth);
+        while (tagIndex !== this.currentItemIndex) {
+            console.log('to: ' + tagIndex);
+            console.log('current: ' + this.currentItemIndex);
+            if (tagIndex > this.currentItemIndex) {
+                this.scrollLeft();
+            } else {
+                this.scrollRight();
+            }
+            console.log('afterscroll,to: ' + tagIndex);
+            console.log('afterscroll,current: ' + this.currentItemIndex);
         }
+        window.location.hash = 'tag-' + tagName;
     }
     init() {
-        console.log('init');
-        this.tagList.style.left = '0px'; 
+        console.log('init: ', this.itemContainer.style.left);
+        if(window.location.hash) {
+            let tagName = window.location.hash.split('#')[1];
+            console.log(tagName);
+            this.currentItemIndex = this.getTagIndexByName(tagName);
+        } else {
+            this.currentItemIndex = 0;
+        }
         this.initItemPosition();
+        console.log('init currentItemIndex: ' + this.currentItemIndex);
+        for(let i=0; i<this.totalTagCounts; i++) {
+            let tagName = this.tagLinks[i].innerHTML;
+            this.tagLinks[i].addEventListener('click', event => {
+                this.scrollToTag(tagName);
+            });
+        }
     }
 }
 
