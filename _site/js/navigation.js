892617c7746bc88c5c334558e24668f4fa575f05
Juuggo <Juuggo.c@gmail.com>
2019-01-31 23:12:33 +0800

    Using media queries to implement responsive page.

--- _site/js/navigation.js
+++ _site/js/navigation.js
@@ -1,3 +1,28 @@
-setTimeout(function() {
-    document.getElementById('right-nav').classList.remove('closed');
-}, 100);
+document.addEventListener('DOMContentLoaded', function() {
+    let navToggleBtn = document.getElementById('toggle-nav');
+    let navBar = document.getElementsByTagName('nav')[0];    
+    navBar.style.right = window.getComputedStyle(navBar).right + 'px';
+    let navToggled = 0;
+    let handleResize = function() {
+        console.log('resize');
+        navToggled = 0;
+        navToggleBtn.classList.remove('toggle');
+        if(document.documentElement.clientWidth > 1023) {
+            navBar.style.right = '0px';
+        } else {
+            navBar.style.right = '-200px';
+        }
+    }
+    window.addEventListener('resize', handleResize);
+    navToggleBtn.addEventListener('click', function(){
+        if(navToggled === 0) {
+            navToggled = 1;
+            navToggleBtn.classList.add('toggle');
+            navBar.style.right = '0px';
+        } else {
+            navToggled = 0;
+            navToggleBtn.classList.remove('toggle');
+            navBar.style.right = '-200px';
+        }
+    })
+})
